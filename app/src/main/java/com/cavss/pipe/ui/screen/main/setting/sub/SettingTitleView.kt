462a59a7c5cb81e25bc0993e883ad4b173c84821
cavss <cavss1118@gmail.com>
2022-05-18 23:18:34 +0900

    010. SettingTitleView 1. 폰트추가가 필요해보임.

---
+++ app/src/main/java/com/cavss/pipe/ui/screen/main/setting/sub/SettingTitleView.kt
@@ -0,0 +1,29 @@
+package com.cavss.pipe.ui.screen.main.setting.sub
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cavss.pipe.R
+import com.cavss.pipe.ui.custom.icontitle.IconTitle
+
+@Composable
+fun SettingTitleView() {
+    IconTitle(
+        setImage = R.drawable.ic_launcher_background,
+        setSize = 40,
+        setTitle = stringResource(id = R.string.setting_title),
+        setTitleColour = Color.White,
+        setTitleSize = 30,
+        setModifier = Modifier
+            .padding(10.dp)
+    )
+}
